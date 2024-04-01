121c185041dcdcecac5bb9dbe1b28f15e8378039
Jiheon <hanjihh1123@gmail.com>
2024-04-01 16:27:44 +0900

    프로젝트 총정리

--- spring07/src/main/java/com/example/demo/controller/BoardController.java
+++ spring07/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,4 +22,29 @@ public class BoardController {
 		Page page = svc.findAll(pageno);
 		return new ModelAndView("board/list").addObject("page", page);
 	}
+
+	@PreAuthorize("isAuthenticated()") // 관리자가 없다면 @Secured("ROLE_USER")랑 똑같음
+	@GetMapping("/board/write")
+	public void write() {
+	}
+
+	@Secured("ROLE_USER") // 관리자가 있다면 관리자는 글쓰기를 하면 안되니까 @Secured로 작업하는게 맞다
+	@PostMapping("/board/write")
+	public ModelAndView write(String content) {
+		System.out.println(content);
+		return null;
+	}
+
+//	@GetMapping("/board/read")
+//	public void read() {
+//	}
+//
+//	@GetMapping("/board/update")
+//	public void update() {
+//	}
+//
+//	@GetMapping("/board/delete")
+//	public void delete() {
+//	}
+
 }
